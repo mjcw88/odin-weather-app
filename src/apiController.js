8b99f2179630fce0d86a7adc1eee3eade31adfa5
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-04-12 11:50:48 +0100

    filled out basic html placeholder layout

--- src/apiController.js
+++ src/apiController.js
@@ -5,17 +5,18 @@ export async function fetchData(query) {
     const KEY = "HRMCPGZE4BZBZDD4FC4UXNJD6";
 
     try {
-        const responseMetric = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=${KEY}&unitGroup=metric`)
-        const dataMetric = await responseMetric.json();
-        const filenameMetric = "celsius";
-        saveToStorage(filenameMetric, dataMetric);
+        const [responseMetric, responseUs] = await Promise.all([
+            fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=${KEY}&unitGroup=metric`),
+            fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=${KEY}&unitGroup=us`)
+        ]);
 
-        const responseUs = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=${KEY}&unitGroup=us`)
+        const dataMetric = await responseMetric.json();
         const dataUs = await responseUs.json();
-        const filenameFahrenheit = "fahrenheit";
-        saveToStorage(filenameFahrenheit, dataUs);
-        
-        updateDisplay(filenameMetric);
+
+        saveToStorage('celsius', dataMetric);
+        saveToStorage('fahrenheit', dataUs);
+
+        updateDisplay('celsius');
     } catch (error) {
         console.error(error);
     }
